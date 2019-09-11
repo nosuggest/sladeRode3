bcd5358cc578726b6d4d626a05bb6cd08a7a27a9
shataowei <taowei.sha@ymm56.com>
2019-09-11 12:14:42 +0800

    list

---
+++ Chapter02/cmake-build-debug/CMakeFiles/Chapter02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter02.dir/main.cpp.o"
+  "Chapter02.pdb"
+  "Chapter02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter02/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Case00.cpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    case00();
+    return 0;
+}

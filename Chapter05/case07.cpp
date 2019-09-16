144e949584a055c214c7cd10f40407f6c4602c2c
shataowei <taowei.sha@ymm56.com>
2019-09-17 00:18:12 +0800

    指针函数

--- Chapter05/case07.cpp
+++ Chapter05/case07.cpp
@@ -7,10 +7,10 @@
 
 using namespace std;
 //函数指针的定义方式
-/*第一种*/
-auto prcCalc0 = calcAdd;//必须要初始化
-/*第二种*/
-double (*prcCalc1)(double, double);
-/*第三种*/
-typedef double (*prcCalc2)(double, double);
-prcCalc2 prcCalc21;
+///*第一种*/
+//auto prcCalc0 = calcAdd;//必须要初始化
+///*第二种*/
+//double (*prcCalc1)(double, double);
+///*第三种*/
+//typedef double (*prcCalc2)(double, double);
+//prcCalc2 prcCalc21;

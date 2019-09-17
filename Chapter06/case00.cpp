49ac971c8367257a89cd2cfa0689c6e7a2410ef3
shataowei <taowei.sha@ymm56.com>
2019-09-17 23:30:23 +0800

    inline function

---
+++ Chapter06/case00.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Administrator on 2019/9/17.
+//
+
+#include "iostream"
+// 替换N为6
+#define N 6
+#define S(num) num*num
+
+using namespace std;
+
+// 内联：替换
+inline void case001() {
+    cout << "inline function" << endl;
+    cout << N << endl;
+    cout << S(N) << endl;
+
+    // num*num = 5+10*5+10
+    cout << S(5 + 10) << endl;
+}
--- Chapter06/main.cpp
+++ Chapter06/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "case00.cpp"
 
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    case001();
     return 0;
 }

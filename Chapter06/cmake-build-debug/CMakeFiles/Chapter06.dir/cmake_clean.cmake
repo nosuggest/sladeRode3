e63d493e362f98e1959223c51ff9ad4ae8cea917
shataowei <taowei.sha@ymm56.com>
2019-09-17 23:13:27 +0800

    inline function

---
+++ Chapter06/cmake-build-debug/CMakeFiles/Chapter06.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter06.dir/main.cpp.obj"
+  "Chapter06.pdb"
+  "Chapter06.exe"
+  "Chapter06.exe.manifest"
+  "libChapter06.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

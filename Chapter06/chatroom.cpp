4f8fe2b738034170d24cfb276c4fd528b408ee47
shataowei <taowei.sha@ymm56.com>
2019-09-22 18:31:49 +0800

    函数默认参数

--- Chapter06/chatroom.cpp
+++ Chapter06/chatroom.cpp
@@ -12,9 +12,13 @@ string chatFrom(const string &toName, const string &content);
 
 string chatTo(const string &toName, const string &content) {
     string msg = "[you] quite speak to " + toName + " about: " + content;
-    return msg;
+    return msg;//这样返回的是局部变量，函数调用完会被回收，可能被销毁
 }
 
-string chatFrom() {
-
+int test(int a, int b, int c){
+    return a+b+c;
 }
+// 默认参数必须从右往左定义
+// int test(int a, int b=5, int c=10),可以
+// int test(int a, int b=5, int c),不可以
+// int test(int a=19, int b=5, int c=10),可以
